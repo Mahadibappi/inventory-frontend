41f51a106bcda4e4edbb715bba12a1fa20093f31
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-02-03 18:55:23 +0600

    forgot password created

--- src/services/authService.js
+++ src/services/authService.js
@@ -98,18 +98,18 @@ export const resetPassword = async (userData, resetToken) => {
 };
 
 // Get Login Status
-// export const getLoginStatus = async () => {
-//   try {
-//     const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`);
-//     return response.data;
-//   } catch (error) {
-//     const message =
-//       (error.response && error.response.data && error.response.data.message) ||
-//       error.message ||
-//       error.toString();
-//     toast.error(message);
-//   }
-// };
+export const getLoginStatus = async () => {
+  try {
+    const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`);
+    return response.data;
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
+  }
+};
 // Get User Profile
 export const getUser = async () => {
   try {
