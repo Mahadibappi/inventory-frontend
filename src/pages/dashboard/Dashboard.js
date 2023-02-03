800119c80fc30f03b8e0e0721b6760a84b24bfcc
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-02-03 19:16:34 +0600

    dashboard created

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import ProductList from "../../components/product/productList/ProductList";
 import ProductSummary from "../../components/product/productSummary/ProductSummary";
 import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
-import { getProducts } from "../../redux/features/product/productSlice";
+// import { getProducts } from "../../redux/features/product/productSlice";
 
 const Dashboard = () => {
   useRedirectLoggedOutUser("/login");
@@ -15,15 +15,15 @@ const Dashboard = () => {
     (state) => state.product
   );
 
-  useEffect(() => {
-    if (isLoggedIn === true) {
-      dispatch(getProducts());
-    }
+  // useEffect(() => {
+  //   if (isLoggedIn === true) {
+  //     dispatch(getProducts());
+  //   }
 
-    if (isError) {
-      console.log(message);
-    }
-  }, [isLoggedIn, isError, message, dispatch]);
+  //   if (isError) {
+  //     console.log(message);
+  //   }
+  // }, [isLoggedIn, isError, message, dispatch]);
 
   return (
     <div>
