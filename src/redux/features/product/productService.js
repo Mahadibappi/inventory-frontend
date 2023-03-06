b8d45a8b494953cb8b6fb19d8e23730198b13bfc
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-06 10:01:00 +0600

    HEADERS ADDED

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -6,30 +6,76 @@ const API_URL = `${BACKEND_URL}/api/products/`;
 
 // Create New Product
 const createProduct = async (formData) => {
-  const response = await axios.post(API_URL, formData);
+  const response = await axios.post(API_URL, formData, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+    },
+  });
   return response.data;
 };
 
 // Get all products
 const getProducts = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axios.get(API_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+    },
+  });
   return response.data;
 };
 
 // Delete a Product
 const deleteProduct = async (id) => {
-  const response = await axios.delete(API_URL + id);
+  const response = await axios.delete(API_URL + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+    },
+  });
   return response.data;
 };
 
 // Get a Product
 const getProduct = async (id) => {
-  const response = await axios.get(API_URL + id);
+  const response = await axios.get(API_URL + id, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
+    },
+  });
   return response.data;
 };
 // Update Product
 const updateProduct = async (id, formData) => {
-  const response = await axios.patch(`${API_URL}${id}`, formData);
+  const response = await axios.patch(`${API_URL}${id}`, formData, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+      "Access-Control-Allow-Methods":
+        "GET, HEAD, POST, PATCH, PUT, DELETE, OPTIONS",
+    },
+  });
   return response.data;
 };
 
--- src/redux/features/product/productSlice.js
+++ src/redux/features/product/productSlice.js
@@ -180,7 +180,7 @@ const productSlice = createSlice({
         state.isLoading = false;
         state.isSuccess = true;
         state.isError = false;
-        // console.log(action.payload);
+        console.log(action.payload);
         state.products = action.payload;
       })
       .addCase(getProducts.rejected, (state, action) => {
