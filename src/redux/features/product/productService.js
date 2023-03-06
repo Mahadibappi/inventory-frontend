6e5782ba8aaf21b37cbe6e0cb96e09ff9909ee21
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2023-03-06 12:39:37 +0600

    updated

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -22,7 +22,6 @@ const createProduct = async (formData) => {
 // Get all products
 const getProducts = async () => {
   const response = await axios.get(API_URL, {
-    method: "GET",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
@@ -36,23 +35,13 @@ const getProducts = async () => {
 
 // Delete a Product
 const deleteProduct = async (id) => {
-  const response = await axios.delete(API_URL + id, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
-      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
-    },
-  });
+  const response = await axios.delete(API_URL + id);
   return response.data;
 };
 
 // Get a Product
 const getProduct = async (id) => {
   const response = await axios.get(API_URL + id, {
-    method: "GET",
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
@@ -65,15 +54,13 @@ const getProduct = async (id) => {
 };
 // Update Product
 const updateProduct = async (id, formData) => {
-  const response = await axios.patch(`${API_URL}${id}`, formData, {
-    method: "PATCH",
+  const response = await axios.put(`${API_URL}${id}`, formData, {
     headers: {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers":
         "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
-      "Access-Control-Allow-Methods":
-        "GET, HEAD, POST, PATCH, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, DELETE, OPTIONS",
     },
   });
   return response.data;
